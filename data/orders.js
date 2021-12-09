f872431b4ef42706022d579890422f12c008d326
Tyler Lundgren <tylerlundgren@yahoo.com>
2021-12-08 18:52:16 -0800

    Initial commit for orders subgraph.

---
+++ data/orders.js
@@ -0,0 +1,107 @@
+const orders = [
+    {
+      "id": 1,
+      "products": [
+        7,
+        14,
+        13
+      ],
+      "userId": 9
+    },
+    {
+      "id": 2,
+      "products": [
+        4
+      ],
+      "userId": 5
+    },
+    {
+      "id": 3,
+      "products": [
+        10
+      ],
+      "userId": 3
+    },
+    {
+      "id": 4,
+      "products": [
+        13,
+        7
+      ],
+      "userId": 1
+    },
+    {
+      "id": 5,
+      "products": [
+        7
+      ],
+      "userId": 10
+    },
+    {
+      "id": 6,
+      "products": [
+        12,
+        14,
+        1
+      ],
+      "userId": 1
+    },
+    {
+      "id": 7,
+      "products": [
+        5,
+        10,
+        13
+      ],
+      "userId": 7
+    },
+    {
+      "id": 8,
+      "products": [
+        5,
+        7
+      ],
+      "userId": 3
+    },
+    {
+      "id": 9,
+      "products": [
+        4,
+        11,
+        1
+      ],
+      "userId": 9
+    },
+    {
+      "id": 10,
+      "products": [
+        11
+      ],
+      "userId": 3
+    },
+    {
+      "id": 11,
+      "products": [
+        4
+      ],
+      "userId": 2
+    },
+    {
+      "id": 12,
+      "products": [
+        1,
+        15
+      ],
+      "userId": 3
+    },
+    {
+      "id": 13,
+      "products": [
+        13,
+        10
+      ],
+      "userId": 10
+    }
+]
+
+module.exports = orders;
---
+++ index.js
@@ -0,0 +1,44 @@
+// Dependencies
+const fs = require("fs");
+const { ApolloServer, gql } = require("apollo-server");
+const { buildSubgraphSchema } = require("@apollo/federation")
+
+// Internal imports
+const typeDefs = gql(fs.readFileSync("./orders.graphql", 'utf8'));
+const orders = require("./data/orders.js");
+
+// Variable Definitions
+const port = process.env.PORT || 4004
+
+const resolvers = {
+    User: {
+        orders(user) {
+            return orders.filter((order) => order.userId === parseInt(user.id));
+        }
+    },
+    Order: {
+        products(order) {
+            return order.products.map((product) => ({ __typename: "Product", id: product }));
+        },
+        user(order) {
+            return { __typename: "User", id: order.userId };
+        }
+    },
+    Query: {
+        order(_, { id }) {
+            return orders.find((order) => order.id === parseInt(id, 10));
+        },
+        orders() {
+            return orders
+        }
+    }
+};
+
+// Apollo Server Setup
+const server = new ApolloServer({
+    schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
+});
+  
+server.listen({ port }).then(({ url }) => {
+    console.log(`Products service ready at ${url}`);
+});
